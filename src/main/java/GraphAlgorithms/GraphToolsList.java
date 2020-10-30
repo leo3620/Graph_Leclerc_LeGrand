52de9cdf4a996cce246085452a84c1405a4ab864
Leo LECLERC <leo.leclerc@imt-atlantique.net>
2020-10-30 14:02:45 +0100

    Question 22 : Composantes connexes

--- src/main/java/GraphAlgorithms/GraphToolsList.java
+++ src/main/java/GraphAlgorithms/GraphToolsList.java
@@ -1,27 +1,37 @@
 package GraphAlgorithms;
 
+import static java.util.stream.Collectors.toList;
+
 import Abstraction.AbstractListGraph;
 import AdjacencyList.DirectedGraph;
 import Nodes.AbstractNode;
+import Nodes.DirectedNode;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Queue;
+import java.util.Set;
 
-public class GraphToolsList  extends GraphTools {
+public class GraphToolsList extends GraphTools {
 
-	private static final int _DEBBUG =0;
+	private static final int _DEBBUG = 0;
 
 	private static int[] visite;
 	private static int[] debut;
-	private static int[] fin;
+	private static Map<DirectedNode, Integer> fin;
 	private static List<Integer> order_CC;
-	private static int cpt=0;
+	private static int cpt = 0;
 
 	//--------------------------------------------------
 	// 				Constructors
 	//--------------------------------------------------
 
-	public GraphToolsList(){
+	public GraphToolsList() {
 		super();
 	}
 
@@ -79,6 +89,64 @@ public class GraphToolsList  extends GraphTools {
 		}
 	}
 
+	public static void composanteConnexe(AbstractListGraph<AbstractNode> graph) {
+		Set<AbstractNode> marked = new HashSet<>();
+
+	}
+
+	static void explorerSommet(AbstractNode s, Set<AbstractNode> a,
+			List<AbstractNode> uneComposante) {
+		a.add(s);
+		uneComposante.add(s);
+		visite[s.getLabel()] = 1;
+		debut[s.getLabel()] = cpt;
+		cpt++;
+		for (AbstractNode t : s.getSuccOrNeighbour()) {
+			if (!a.contains(t)) {
+				explorerSommet(t, a, uneComposante);
+			}
+		}
+		visite[s.getLabel()] = 2;
+		fin.put((DirectedNode) s, cpt);
+		cpt++;
+	}
+
+	static List<List<AbstractNode>> explorerGraphe(DirectedGraph graph, List<DirectedNode> sommets) {
+		Set<AbstractNode> atteint = new HashSet<AbstractNode>();
+
+		debut = new int[graph.getNbNodes()];
+		visite = new int[graph.getNbNodes()];
+		fin = new HashMap<>();
+		cpt = 0;
+		List<List<AbstractNode>> composante = new ArrayList<>();
+
+		for (AbstractNode s : sommets) {
+			if (!atteint.contains(s)) {
+				ArrayList<AbstractNode> uneComposante = new ArrayList<>();
+				composante.add(uneComposante);
+				explorerSommet(s, atteint, uneComposante);
+			}
+		}
+
+		return composante;
+	}
+
+	static void getConnex(DirectedGraph graph) {
+		explorerGraphe(graph, graph.getNodes());
+		DirectedGraph graphInverse = (DirectedGraph) graph.computeInverse();
+
+		// Inverse the map
+		List<DirectedNode> sorted = fin
+				.entrySet()
+				.stream()
+				.sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+				.map(Entry::getKey)
+				.collect(toList());
+
+		System.out.println(explorerGraphe(graphInverse, sorted));
+	}
+
+
 	public static void main(String[] args) {
 		int[][] Matrix = GraphTools.generateGraphData(10, 20, false, false, true, 100001);
 		GraphTools.afficherMatrix(Matrix);
@@ -89,7 +157,9 @@ public class GraphToolsList  extends GraphTools {
 		System.out.println("\n=======================================\n");
 		System.out.print("Depth First Search : ");
 		depthFirstSearch(al);
-
+		System.out.println("\n=======================================\n");
+		System.out.print("Get connexity : ");
+		getConnex((DirectedGraph) al);
 		// A completer
 	}
 }
